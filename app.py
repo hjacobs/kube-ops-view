a2a7f0ee1f95efe1fe251eb1e1722f28d89f3072
Henning Jacobs <henning@jacobs1.de>
2016-12-12 23:59:12 +0100

    draw pod rects

--- app.py
+++ app.py
@@ -21,9 +21,15 @@ def get_clusters():
     response = session.get('http://localhost:8001/api/v1/nodes', timeout=5)
     response.raise_for_status()
     nodes = []
+    nodes_by_name = {}
     for node in response.json()['items']:
-        print(node)
-        nodes.append({'name': node['metadata']['name'], 'labels': node['metadata']['labels'], 'status': node['status']})
+        obj = {'name': node['metadata']['name'], 'labels': node['metadata']['labels'], 'status': node['status'], 'pods': []}
+        nodes.append(obj)
+        nodes_by_name[obj['name']] = obj
+    response = session.get('http://localhost:8001/api/v1/pods', timeout=5)
+    response.raise_for_status()
+    for pod in response.json()['items']:
+        nodes_by_name[pod['spec']['nodeName']]['pods'].append(pod)
     return {'kubernetes_clusters': [{'nodes': nodes}]}
 
 
