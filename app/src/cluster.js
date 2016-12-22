8c22ea176efec32bcb338507432bb8b250561719
Shuaib Yunus <shuaib.yunus@zalando.de>
2016-12-22 11:17:26 +0100

    Add menubar and color constants file

--- app/src/cluster.js
+++ app/src/cluster.js
@@ -1,5 +1,6 @@
 import Node from './node.js'
-import {Pod} from './pod.js'
+import { Pod } from './pod.js'
+import { PRIMARY_VIOLET } from './colors.js'
 const PIXI = require('pixi.js')
 
 export default class Cluster extends PIXI.Graphics {
@@ -37,12 +38,12 @@ export default class Cluster extends PIXI.Graphics {
             rows[0] += 20
         }
 
-        this.lineStyle(2, 0xaaaaff, 1)
+        this.lineStyle(2, PRIMARY_VIOLET, 1)
         const width = Math.max(rows[0], rows[1])
         this.drawRect(0, 0, width, nodeBox.height * 2 + 30)
 
         var topHandle = new PIXI.Graphics()
-        topHandle.beginFill(0xaaaaff, 1)
+        topHandle.beginFill(PRIMARY_VIOLET, 1)
         topHandle.drawRect(0, 0, width, 15)
         topHandle.endFill()
         var text = new PIXI.Text(this.cluster.api_server_url, {fontFamily: 'ShareTechMono', fontSize: 10, fill: 0x000000})
