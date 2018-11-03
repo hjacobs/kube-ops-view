384ea8f06d85aae97dcf6f622e124aeb5bd560e7
Warren Seymour <warren@fountainhead.tech>
2018-11-03 11:03:07 +0000

    Replace Heapster with Metrics API (#177)
    * Replace Heapster with Metrics API call
    
    * Add `n` factor
    
    Metrics API likes to report CPU usage in `n`-suffixed units, so make
    sure its represented correctly.

--- kube_ops_view/kubernetes.py
+++ kube_ops_view/kubernetes.py
@@ -101,7 +101,7 @@ def query_kubernetes_cluster(cluster):
             unassigned_pods[pod_key] = obj
 
     try:
-        response = request(cluster, '/api/v1/namespaces/kube-system/services/heapster/proxy/apis/metrics/v1alpha1/nodes')
+        response = request(cluster, '/apis/metrics.k8s.io/v1beta1/nodes')
         response.raise_for_status()
         data = response.json()
         if not data.get('items'):
@@ -112,7 +112,7 @@ def query_kubernetes_cluster(cluster):
     except Exception as e:
         logger.warning('Failed to query node metrics {}: {}'.format(cluster.id, get_short_error_message(e)))
     try:
-        response = request(cluster, '/api/v1/namespaces/kube-system/services/heapster/proxy/apis/metrics/v1alpha1/pods')
+        response = request(cluster, '/apis/metrics.k8s.io/v1beta1/pods')
         response.raise_for_status()
         data = response.json()
         if not data.get('items'):
